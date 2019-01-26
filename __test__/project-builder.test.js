852b0a1d5a85dbf4d679f5f63ad464887e9de935
Jason <jikurata@gmail.com>
2019-01-26 11:33:38 -0800

    Add unit test for pathInfo

--- __test__/project-builder.test.js
+++ __test__/project-builder.test.js
@@ -1,5 +1,8 @@
 'use strict';
+const fs = require('fs');
 const ProjectBuilder = require('../lib/ProjectBuilder.js');
+const Logger = require('@jikurata/logger');
+Logger.printMessage = false;
 
 describe('Project Builder functional tests', () => {
   describe('use adds a build handler to builder\'s middleware', () => {
@@ -7,6 +10,83 @@ describe('Project Builder functional tests', () => {
       const builder = new ProjectBuilder();
       builder.use((curr, next) => {});
       expect(builder.middleware.length).toBe(1);
+    });
+  });
+  describe('Resolves src paths to their intended build path', () => {
+    test('Returns app/client/script/foo.js', () => {
+      const builder = new ProjectBuilder('app/client', 'src');
+      const path = builder.resolveToDest('src/script/foo.js');
+      expect(path).toEqual('app/client/script/foo.js');
+    });
+    test('Returns app/client/script/foo.js', () => {
+      const builder = new ProjectBuilder('app/client', ['src', 'lib']);
+      const path = builder.resolveToDest('lib/script/foo.js');
+      expect(path).toEqual('app/client/script/foo.js');
+    });
+    test('Returns app/client/script/foo.js', () => {
+      const builder = new ProjectBuilder('app/client', ['src', 'lib/script', 'lib']);
+      const path = builder.resolveToDest('lib/script/foo.js');
+      expect(path).toEqual('app/client/script/foo.js');
+    });
+    test('Returns app/client/script/foo.js', () => {
+      const builder = new ProjectBuilder('app/client', ['src', 'lib']);
+      const path = builder.resolveToDest('dev/src/script/foo.js');
+      expect(path).toEqual('app/client/script/foo.js');
+    });
+  });
+  describe('Converts a path into structured info', () => {
+    test('returns name as foo.bar', () => {
+      const builder = new ProjectBuilder('dist', 'src');
+      const info = builder.pathInfo('src/script/foo.bar.js');
+      expect(info.name).toEqual('foo.bar');
+    });
+    test('returns name as foo.bar.bundle', () => {
+      const builder = new ProjectBuilder('dist', 'src');
+      const info = builder.pathInfo('src/script/foo.bar.bundle.js');
+      expect(info.name).toEqual('foo.bar.bundle');
+    });
+    test('returns name as .config', () => {
+      const builder = new ProjectBuilder('dist', 'src');
+      const info = builder.pathInfo('src/.config');
+      expect(info.name).toEqual('.config');
+    });
+    test('returns type as js', () => {
+      const builder = new ProjectBuilder('dist', 'src');
+      const info = builder.pathInfo('src/script/foo.bar.js');
+      expect(info.type).toEqual('js');
+    });
+    test('returns dest as dist/script/foo.bar.js', () => {
+      const builder = new ProjectBuilder('dist', 'src');
+      const info = builder.pathInfo('src/script/foo.bar.js');
+      expect(info.dest).toEqual('dist/script/foo.bar.js');
+    });
+  });
+  describe('Clears dest path of any existing files', () => {
+    test('Expect build location to be empty', (done) => {
+      const builder = new ProjectBuilder('__test__/example/dist');
+      builder.clearDest()
+      .then(() => {
+        expect(fs.readdirSync('__test__/example/dist').length).toEqual(0)
+        done();
+      })
+      .catch(err => console.error(err));
+    });
+  });
+  describe('Builds src files to dest', () => {
+    test('Expect build location to have two files', (done) => {
+      const builder = new ProjectBuilder('__test__/example/dist', '__test__/example/src');
+      builder.use((curr, next) => {
+        if ( curr.type === 'html' || curr.type === 'js' ) {
+          fs.copyFileSync(curr.path, curr.dest);
+        }
+        return next();
+      });
+      builder.build()
+      .then(() => {
+        expect(fs.readdirSync('__test__/example/dist').length).toEqual(2)
+        done();
+      })
+      .catch(err => console.error(err));
     });
   });
 });
--- readme.md
+++ readme.md
@@ -1,7 +1,99 @@
 # build-project v0.0.0
-Build your project with grace and elegance
+Use your preferred build tools to build your project
 ---
 ## Install
 ```
 npm install @jikurata/build-project
+```
+## Usage
+Add your build tools as middleware
+```
+const ProjectBuilder = require('@jikurata/build-project');
+const fs = require('fs');
+const browserify = require('browserify');
+
+const builder = new ProjectBuilder('dist', 'src');
+
+// Adds browserify as middleware
+builder.use((curr, next) => {
+    if ( curr.filetype !== 'js' ) return next();
+    let b = browserify(curr.path);
+    b.bundle((err, buffer) => {
+        if ( err ) return console.error(err);
+        fs.writeFileSync(buffer, curr.dest);
+    });
+});
+
+// Executes the build process
+builder.build();
+```
+Deconstructing a middleware operation
+```
+const middleware = (curr, next) => {
+    // Do things
+};
+```
+- **curr**: is an object containing basic information about the current file being processed.
+    - Properties:
+        - *path*: Contains the full root-relative path of the current file
+        - *name*: Contains the file's base name
+        - *type*: Contains the file's extension
+
+## Example
+Project structure:
+- src/index.html
+- src/asset/scss/style.scss
+- src/asset/script/main.js
+```
+const ProjectBuilder = require('@jikurata/build-project');
+const fs = require('fs');
+
+// Build tools
+const browserify = require('browserify');
+const sass = require('node-sass');
+
+const builder = new ProjectBuilder('dist', 'src');
+
+// Define middleware operation for html files
+builder.use((curr, next) => {
+    if ( curr.type !== 'html' ) return next();
+
+    fs.copyFileSync(curr.path, curr.dest);
+});
+
+// Define middleware operation for js files
+builder.use((curr, next) => {
+    if ( curr.type !== 'js' ) return next();
+
+    // Correct build path destination
+    curr.dest.replace(curr.name, `${curr.name}.bundle`);
+
+    let b = browserify(curr.path);
+    b.bundle((err, buffer) => {
+        if ( err ) return console.error(err);
+        fs.writeFileSync(buffer, curr.dest);
+    });
+})
+
+// Define middleware operation for scss files
+builder.use((curr, next) => {
+    if ( curr.type !== 'scss' ) return next();
+    
+    // Correct the build path destination
+    curr.dest.replace('scss', 'css');
+
+    const content = sass.renderSync({
+        file: curr.path,
+        sourceMap: false,
+        outputStyle: 'compressed'
+    });
+    fs.writeFileSync(content.css, curr.dest);
+});
+
+builder.build();
+
+// Resulting build:
+- dist/index.html
+- dist/asset/css/style.css
+- dist/asset/script/main.bundle.js
 ```
